a4f4c35cb943e2d29293bc404f0acab6ae0f504e
Yotam Bentov <yotam@bloomapi.com>
2019-07-08 12:19:06 -0700

    get java to build

--- android/src/main/java/com/rnim/rn/audio/ReactNativeAudioPackage.java
+++ android/src/main/java/com/rnim/rn/audio/ReactNativeAudioPackage.java
@@ -25,6 +25,11 @@ public class ReactNativeAudioPackage implements ReactPackage {
         return modules;
     }
 
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     /**
      * @param reactContext
      * @return a list of view managers that should be registered with {UIManagerModule}
